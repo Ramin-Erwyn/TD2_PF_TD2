b417301c3609b04316f0de2bfc4f7f7c1d72be60
Erwyn <r.erwyn@gmail.com>
2021-10-25 17:01:33 +0200

    exo2

--- src/main/java/td2/App.java
+++ src/main/java/td2/App.java
@@ -46,6 +46,24 @@ public class App {
         Predicate<Double> poidsTropLourd = poids -> poids > 150.0;
         Predicate<Double> poidsCorrecte = poidsTropLourd.negate();
         Predicate<Paire<Integer, Double>> accesAutorise = acces -> tailleCorrecte.test(acces.fst) && poidsCorrecte.test(acces.snd);
+
+        // refaire avec Paire
+
+        Predicate<Paire<Integer,Double>> tropPetitPaire = taille -> taille.fst < 100; // int ou double
+        Predicate<Paire<Integer,Double>> tropGrandPaire = taille -> taille.fst > 200; // int ou double
+        Predicate<Paire<Integer,Double>> mauvaiseTaillePaire = tropPetitPaire.or(tropGrandPaire); // int ou double
+        Predicate<Paire<Integer,Double>> TailleCorrectePaire = mauvaiseTaillePaire.negate(); // int ou double
+        Predicate<Paire<Integer,Double>> tropLourdPaire = x -> x.snd > 150.0; // int ou double
+        Predicate<Paire<Integer,Double>> PoidsCorrectePaire = tropLourdPaire.negate(); // int ou double
+        Predicate<Paire<Integer,Double>> accesAutoriséPaire = TailleCorrectePaire.and(PoidsCorrectePaire); // int ou double
+
+       // POUR LES TESTS
+       // (210,-) -> c'est pas bon
+       // (90,-) -> c'est pas bon
+       // (-,160) -> c'est pas bon
+       // (150, 100) -> c'est bon
+        // Mais on remarque c'est ça fait un polyhèdre (200,150) (100,150) sont les bornes
+
     }
     public static void main(String[] args) {
         System.out.println("-------Q1.1--------");
