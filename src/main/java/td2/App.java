d5cde9ebeab520dc593dd526c0aa8eb300266149
Erwyn <r.erwyn@gmail.com>
2021-10-25 17:41:21 +0200

    exo3

--- src/main/java/td2/App.java
+++ src/main/java/td2/App.java
@@ -1,7 +1,14 @@
 package td2;
 
+import td2.universite.Annee;
+import td2.universite.Etudiant;
+import td2.universite.Matiere;
+import td2.universite.UE;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 
 public class App {
@@ -65,9 +72,63 @@ public class App {
         // Mais on remarque c'est ça fait un polyhèdre (200,150) (100,150) sont les bornes
 
     }
+
+ //EXO 3
+
+    public static void question3(){
+        Matiere m1 = new Matiere("MAT1");
+        Matiere m2 = new Matiere("MAT2");
+        UE ue1 = new UE("UE1", Map.of(m1, 2, m2, 2));
+        Matiere m3 = new Matiere("MAT3");
+        UE ue2 = new UE("UE2", Map.of(m3, 1));
+        Annee a1 = new Annee(Set.of(ue1, ue2));
+        Etudiant e1 = new Etudiant("39001", "Alice", "Merveille", a1);
+        e1.noter(m1, 12.0);
+        e1.noter(m2, 14.0);
+        e1.noter(m3, 10.0);
+        System.out.println(e1);
+        Etudiant e2 = new Etudiant("39002", "Bob", "Eponge", a1);
+        e2.noter(m1, 14.0);
+        e2.noter(m3, 14.0);
+        Etudiant e3 = new Etudiant("39003", "Charles", "Chaplin", a1);
+        e3.noter(m1, 18.0);
+        e3.noter(m2, 5.0);
+        e3.noter(m3, 14.0);
+
+    }
+    //boucle for
+    public void afficheSi(String entete, Predicate<Etudiant> rtr, Annee a) {
+
+        for (Etudiant e : a.etudiants()){
+            if (rtr.test(e)){
+                System.out.println(e);
+            }
+        }
+        System.out.println(String.format(" %s\n", entete));
+    }
+    // utilisation du foreach
+    public void afficheSi1(String entete, Predicate<Etudiant> T, Annee a) {
+        System.out.println(String.format(" %s\n", entete));
+        a.etudiants().forEach(e -> {
+            if (T.test(e)){
+                System.out.println(e);
+            }
+
+        });
+    }
+    public  final Predicate<Etudiant> aDEF = e -> {
+        for (UE ue : e.annee().ues()) {
+            //...
+        }
+        return false;
+    };
     public static void main(String[] args) {
         System.out.println("-------Q1.1--------");
         q1a();
         //q1b
+        question2();
+        question3();
+
+
     }
 }
